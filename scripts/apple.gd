dd482af420628342a583d4f86bfd6b763eedceed
Tianzhe Sun <tianzhs@uci.edu>
2024-11-07 19:42:33 -0800

    created tutorial and redesigned map

--- scripts/apple.gd
+++ scripts/apple.gd
@@ -1,9 +1,15 @@
 extends Area2D
 
+const COINS_PER_APPLE = 3
+
 @onready var animation_player = $AnimationPlayer
+@onready var game_manager = %GameManager
 
 func _on_body_entered(body):
 	if body.hp < body.MAX_HP:
 		body.hp += 1
+	else:
+		for i in range(COINS_PER_APPLE):
+			game_manager.add_pt()
+		
 	animation_player.play("eating")
-

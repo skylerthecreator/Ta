b2743a72ef55f058325e6c430f4e59882310e6f2
Tianzhe Sun <tianzhs@uci.edu>
2024-11-21 01:35:01 -0800

    map updates

--- scripts/dialogue1.gd
+++ scripts/dialogue1.gd
@@ -8,10 +8,9 @@ const dialogue_lines = ["Ah, look who's finally awake!",
 "You don't remember who or where you are?...",
 "You have suffered a great injury. You must've lost your memory from it.",
 "I prepared for you a healing potion next to the bed. Don't worry, it's a family recipe!",
-"I guess I shall reintroduce myself. My name is Maurice. My objective is to preserve peace across the land.",
-"I'm afraid for reasons I cannot say, I am unable to tell you who you are, nor how you know me.",
-"Although, if you follow the path in front of you, I believe you may regain your memory.",
-"Until then, take my amulet on that table. It will provide you with a layer of great protection."]
+"I'm afraid for reasons I cannot say, I am unable to tell you who you are, nor who I am.",
+"Although, if you follow the path in front of you, you may regain your memory, but I must warn you, it is not a path that everyone survives.",
+"Take my amulet on that table. It will provide you with a layer of great protection."]
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and !started:

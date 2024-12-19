4b97c4e477047cb52a44b05740d1c993cf78fd52
Tianzhe Sun <tianzhs@uci.edu>
2024-12-18 23:45:49 -0800

    title and boss2

--- scripts/actualslime.gd
+++ scripts/actualslime.gd
@@ -1,15 +1,20 @@
 extends Area2D
 @onready var AS = $AnimatedSprite2D
 @onready var CS = $CollisionShape2D
+@onready var down = $down
 
 var SPEED = 60
 var direction = 0
 # Called when the node enters the scene tree for the first time
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if direction == 1 and scale.x > 0:
+		scale.x *= -1
 	if AS.animation != "die":
 		AS.play("default")
 		position.x += delta * SPEED * direction
+		if !down.is_colliding():
+			position.y += delta * SPEED
 	else:
 		if !AS.is_playing():
 			queue_free()
